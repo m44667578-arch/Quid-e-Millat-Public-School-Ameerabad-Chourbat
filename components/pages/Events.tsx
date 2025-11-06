59db1357c4ffa81e292e443b6efecd3d1e2c8701
LATIF COMPUTERS <m44667578@gmail.com>
2025-11-07 00:03:13 +0500

    Added the posterUrl

--- components/pages/Events.tsx
+++ components/pages/Events.tsx
@@ -19,6 +19,7 @@ export const eventsData: SchoolEvent[] = [
     detailedDescription: 'This sacred day reminds us of the values taught by the Prophet Muhammad (PBUH) peace, kindness, justice, patience, humility, and love for humanity. Muslims offer naat, recite Durood-o-Salam, arrange gatherings of remembrance, distribute food, and perform acts of charity to follow the Sunnah of the Prophet (PBUH).',
     mediaUrl: '/images/events/video/Eid.mp4',
     mediaType: 'video',
+    posterUrl: '/images/events/video/Eid.mp4',
   },
   {
     id: 3,
